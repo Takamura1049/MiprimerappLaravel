3eb357d65558b8961d65d475d619db0923e432ce
Landon Emiliano Hernández Bonilla <landonemilianoh@gmail.com>
2026-09-11 01:11:34 +0000

    Mi nuevo cambio

--- laravel-app/resources/views/welcome.blade.php
+++ laravel-app/resources/views/welcome.blade.php
@@ -1,15 +1,24 @@
 <?php
     echo"<h1> Mi primer pagina Web en Laravel </h1>";
     $x = 'Hola Mundo';
-// $y = ['Hola', 'Mundo', 'Laravel'];
+    //$y = ['Hola', 'Mundo', 'Laravel'];
     $y = array('Hola', 'Mundo', 'Laravel');
-    $z = ["marca" => "toyota", "modelo" => "corolla", ];
+    $z = (object)["marca" => "Toyota", "modelo" => "Corolla", "color" => "Rojo"];
+    var_dump($z);
+    var_dump($y);
+    echo "<br>";
     echo "<p>$x</p>";
     echo "<p>$y[0] $y[1] $y[2]</p>";
     echo $y[0] . " " . $y[1] . " " . $y[2];
+    echo "<br>";
+    echo $z->marca . " " . $z->modelo . " " . $z->color;
     echo "<ul>";
         foreach ($y as $item) {
             echo "<li>$item</li>";
         }
         echo "</ul>";
+    echo "<p> La suma de 5 + 10 es :". sumar(5, 10) . "</p>";
+    function sumar($n1, $n2){
+        return $n1 + $n2;
+            }
 ?>
--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -1,7 +1,17 @@
 <?php
-
+ 
 use Illuminate\Support\Facades\Route;
-
+ 
 Route::get('/', function () {
     return view('welcome');
+});
+ 
+ Route::get('/login', function (){
+    $n1 = 5;
+    $n2 = 10;
+    function suma ($n1, $n2){
+        return $n1 + $n2;
+    }
+    return "La suma de $n1 + $n2 es: " . suma($n1, $n2);
+    //return "Login";
 });
